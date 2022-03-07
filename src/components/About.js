f64026e4879bc58fd2734a14c4a926fbe45ffae3
Anant Sharma <ananttheant4444@gmail.com>
2022-03-08 02:25:01 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,40 +3,22 @@ import Section from "./Section";
 const About = () => (
   <Section heading="background" icon="potion">
     <div className="font-light">
-      <div className="mb-4">
-        I'm an independent gameplay and tools engineering contractor, currently
-        based in <span className="font-bold">Edinburgh, UK</span>. I work with a
-        wide variety of clients, from individuals looking for someone to build
-        their dream game to independent studios and startups that need a
-        talented professional to help to develop their projects. I graduated
-        from Birmingham City University, with First-class Honours in BSc.
-        Interactive Entertainment: Game Development, and have commercial AAA
-        programming experience working as an Online Tools Designer at <span className="font-bold">Rockstar
-        North</span>.
+      <div className="mb-4">        
+        I'm a <span className="font-bold">game developer</span>, currently based in <span className="font-bold">New Delhi, India</span>.
+        I've worked with many studios on different aspects of game development with people from various backgrounds both cultural and professional.
       </div>
       <div className="mb-4">
-        As an independent contractor, I take a lot of joy in being able to
-        continually work on a variety of different projects as it forces me to
-        interact with new technologies, work with new people and consistently
-        allow me to keep growing and learning. There's nothing like the freedom
-        of being able to always work on the most interesting up-and-coming
-        opportunities that present themselves to you. My passion truly lies in
-        game development. I love being able to design and develop interactive
-        experiences that can be enjoyed globally by a wide-range of different
-        audiences. I also love being able to create and experiment with the
-        development of indie tools, as it gives me a chance to try and give back
-        to the community I work in and hopefully make the lives of my fellow
-        developers easier.
+        On this pursuit to feed my curiosity, I've enjoyed my time with <span className="font-bold">User Interface</span>,
+        complex <span className="font-bold">animation</span> state machines, <span className="font-bold">gameplay</span>,
+        <span className="font-bold">multiplayer</span> experiences, <span className="font-bold">programming patterns</span> and much more.
       </div>
       <div className="mb-4">
-        If I'm not working hard with clients, I'm probably working on a personal
-        project, participating in a game jam, powerlifting or exploring the
-        gorgeous highlands of my local area.
+        Apart from game dev, I also have interest in archery, gaming, <span className="font-bold">VR</span> and <span className="font-bold">shaders</span>.
       </div>
       <div>
         <span className="bg-yellow-400 font-bold">
-          I'm always on the lookout for new clients with interesting projects to
-          work on, so if you think that's you, send me an email!
+          I'm always looking forward to challenging opportunities,
+          if you think you have something interesting, feel free to reach out to me on email!
         </span>
       </div>
     </div>
